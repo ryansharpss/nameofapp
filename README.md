5abbaabdbfd4f6463b31571e6dc42ebdd472a487
Ryan Sharp <ryansharpss@gmail.com>
2016-08-03 13:42:34 +1000

    3.8 added first commit

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # README
 
 
-My name is Ryan I am learning to become a Front-End web developer.
+My name is Ryan I am learning to become a Front-End web developer. 
+
+I Love to code!
 
 # nameofapp
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+
+  devise_for :users
+  resources :users
+
+  resources :products do
+    resources :comments
+  end
+  resources :orders, only: [:index, :show, :create, :destroy]
+
+  root 'static_pages#index'
+
+  
+
+  get 'index'     => 'static_pages#index'
+
+ 
+
+
+  post 'static_pages/thank_you'
+
+  post 'payments/create'
 end

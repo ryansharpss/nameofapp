9be7e3a98f43f05e57fd53478716eb5dc2ce46ad
Ryan Sharp <ryansharpss@gmail.com>
2016-08-08 15:46:00 +1000

    3:8 static pages update

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,20 @@
 Rails.application.routes.draw do
 
-
-
-  devise_for :users
-  resources :users
-
-  resources :products do
-    resources :comments
-  end
-  resources :orders, only: [:index, :show, :create, :destroy]
-
-  root 'static_pages#index'
-
-  
-
-  get 'index'     => 'static_pages#index'
-
  
 
+  get 'static_pages/index'
+  get 'about', to: 'static_pages#about'
+  get 'contact', to: 'static_pages#contact'
+  
+  
+  
 
-  post 'static_pages/thank_you'
 
-  post 'payments/create'
+# You can have the root of your site routed with "root"
+
+  root 'static_pages#index'
+  
+
+  
+  resources :orders, only: [:index, :show, :create, :destroy]
 end

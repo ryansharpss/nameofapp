b716ae24bf21c9e7065716f3627d7cca3cc9594c
Ryan Sharp <ryansharpss@gmail.com>
2016-08-26 19:09:03 +1000

    updated changes on exercise 4:3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 end
+
+class OrdersController < ApplicationController
+
+   def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+end
